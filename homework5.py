3efa1fe6bc8ffde3f162e6869e2e6551d665b388
sreekalababy-crypto <sreekalababy@gmail.com>
2025-10-07 12:46:01 +0530

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,4 +1,5 @@
 import random
+# RPG BATTLE
 ch = ['dragon','goblin','trolls','ghost','skeleton','spider']
 enemy = random.choice(ch)
 player = input('Enter your name : ')
@@ -11,19 +12,22 @@ print(f'{player} Health point :{php} \n {enemy} Health point : {ehp}')
 while php >= 0 and ehp >= 0 :
     print('Turn',turn)
     pch = random.choice(p)
-    if (pch == 'attack') :
+    # ATTACKING EACH OTHER
+    if (pch == 'attack') : 
         edamage = random.randint(10,30)
         ehp = ehp - edamage
         pdamage = random.randint(10,35)
         php = php - pdamage
         print(f'{player} Health point :',{php})
         print(f'{enemy} Health point :',{ehp})
+        # PLAYER DEFENDING
     elif (pch == 'defend') :
         pdamage = random.randint(10,35)
         pdamage = pdamage // 2
         php = php - pdamage
         print(f'{player} Health point :',{php})
         print(f'{enemy} Health point :',{ehp})
+        # PLAYER HEALING
     elif (pch == 'heal') :
         pheal = random.randint(10,25)
         if php + pheal < 100 :
@@ -39,3 +43,4 @@ if php <= 0 :
 elif ehp <= 0 :
     print(f'GAME OVER !!! \n {player} Wins !!')
  
+
